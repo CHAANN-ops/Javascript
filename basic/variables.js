5e6bec038738e4c6f04cbce09a93700cfc8c4e42
CHAANN <hackesquad123@gmail.com>
2024-09-11 14:39:48 +0000

    variables

---
+++ basic/variables.js
@@ -0,0 +1,11 @@
+const accountid = 1223;
+let state = 'Assam';
+var address = 'orang';
+
+console.log(accountid)
+state = 'Delhi'
+console.log(state)
+
+// const cannot be changed they are constant.
+// let is most suitable in modern javascript.
+// var is used but not that suitable because it changes the original value when it is used in scope ({})
