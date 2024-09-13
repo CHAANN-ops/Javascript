06ad9c6c065fc9f2347a809da49c6392e4402791
CHAANN <hackesquad123@gmail.com>
2024-09-13 07:22:57 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // uses new version of javascript.
+// code readabilty should be high and shouuld be future proof;
+const accountid = 1223;
+let state = 'Assam';
+var address = 'orang';
+let contact = 6000161978
+let age;
+let temp = null;
+let login = false
+
+console.log(typeof (state))
+console.log(typeof (contact))
+console.log(typeof undefined) // undefined is undefined datatype only
+console.log(typeof null) // null is a standalone value but it is object datatype
+console.log(typeof (login))
+
+/*
+Data types:
+1. string
+2. number
+3. boolean
+4. undefined
+5. object
+6. symbol (mostly used in react)
+7. null
+8. bigInt
+*/
--- basic/variables.js
+++ basic/variables.js
@@ -9,5 +9,5 @@ state = 'Delhi'
 console.log(state)
 // const cannot be changed they are constant.
 // let is most suitable in modern javascript.
-// var is used but not that suitable because it changes the original value when it is used in scope ({})
+// var is used but not that suitable because of the issue of block scope and functional scope.
 console.table([accountid, state, address, contact, age, temp]) // Gives output in tabular form.
