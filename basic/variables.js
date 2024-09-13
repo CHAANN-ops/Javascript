3041a02d63a23b3932be491d552416fc91b7692c
CHAANN <hackesquad123@gmail.com>
2024-09-13 06:25:20 +0000

    variables

--- basic/variables.js
+++ basic/variables.js
@@ -2,11 +2,12 @@ const accountid = 1223;
 let state = 'Assam';
 var address = 'orang';
 let contact = 6000161978
+let age;
+let temp = null;
 console.log(accountid)
 state = 'Delhi'
 console.log(state)
-console.table([accountid, state, address, contact]) // Gives output in tabular form.
-
 // const cannot be changed they are constant.
 // let is most suitable in modern javascript.
 // var is used but not that suitable because it changes the original value when it is used in scope ({})
+console.table([accountid, state, address, contact, age, temp]) // Gives output in tabular form.
